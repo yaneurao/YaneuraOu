c685eb18cd3d6b07bed583480744f9a844d7c13e
yaneurao <yaneurao@hotmail.com>
2016-04-19 13:54:24 +0900

    V2.81 gen2

--- source/engine/2016-mid-engine/parameters.h
+++ source/engine/2016-mid-engine/parameters.h
@@ -7,16 +7,16 @@
 
 // 深さに比例したfutility pruning
 // depth手先で評価値が変動する幅が = depth * PARAM_FUTILITY_MARGIN_DEPTH
-// [PARAM] min:50,max:120,step:5,interval:1,time_rate:1
-PARAM_DEFINE PARAM_FUTILITY_MARGIN_ALPHA = 90;
+// [PARAM] min:50,max:120,step:2,interval:1,time_rate:1
+PARAM_DEFINE PARAM_FUTILITY_MARGIN_ALPHA = 87;
 
 // 静止探索でのfutility pruning
-// [PARAM] min:50,max:150,step:5,interval:1,time_rate:1
-PARAM_DEFINE PARAM_FUTILITY_MARGIN_QUIET = 128;
+// [PARAM] min:50,max:150,step:2,interval:1,time_rate:1
+PARAM_DEFINE PARAM_FUTILITY_MARGIN_QUIET = 130;
 
 // futility pruningが適用されるdepth。これ以下のdepthに対して適用される。
 // [PARAM] min:5,max:13,step:1,interval:2,time_rate:1
-PARAM_DEFINE PARAM_FUTILITY_RETURN_DEPTH = 7;
+PARAM_DEFINE PARAM_FUTILITY_RETURN_DEPTH = 8;
 
 // 親nodeでのfutilityを行なうdepthとそのmarginと、seeが負の指し手の枝刈りをするdepth
 
@@ -27,7 +27,7 @@ PARAM_DEFINE PARAM_FUTILITY_AT_PARENT_NODE_DEPTH = 7;
 PARAM_DEFINE PARAM_FUTILITY_AT_PARENT_NODE_MARGIN = 170;
 
 // [PARAM] min:2,max:10,step:1,interval:2,time_rate:1
-PARAM_DEFINE PARAM_FUTILITY_AT_PARENT_NODE_SEE_DEPTH = 4;
+PARAM_DEFINE PARAM_FUTILITY_AT_PARENT_NODE_SEE_DEPTH = 5;
 
 //
 // null move dynamic pruning
@@ -40,7 +40,7 @@ PARAM_DEFINE PARAM_FUTILITY_AT_PARENT_NODE_SEE_DEPTH = 4;
 PARAM_DEFINE PARAM_NULL_MOVE_DYNAMIC_ALPHA = 823;
 
 // [PARAM] min:50,max:100,step:4,interval:1,time_rate:1
-PARAM_DEFINE PARAM_NULL_MOVE_DYNAMIC_BETA = 67;
+PARAM_DEFINE PARAM_NULL_MOVE_DYNAMIC_BETA = 69;
 
 // null moveの前提depthと、beta値を上回ったときにreturnするdepth
 // [PARAM] min:4,max:10,step:1,interval:2,time_rate:1
@@ -97,11 +97,11 @@ PARAM_DEFINE PARAM_REDUCTION_BY_HISTORY = 14980;
 
 // return (Value)(PARAM_RAZORING_MARGIN + PARAM_RAZORING_ALPHA * static_cast<int>(d));
 
-// [PARAM] min:64,max:1024,step:32,interval:1,time_rate:1
+// [PARAM] min:64,max:1024,step:8,interval:1,time_rate:1
 PARAM_DEFINE PARAM_RAZORING_MARGIN = 512;
 
-// [PARAM] min:4,max:32,step:2,interval:1,time_rate:1
-PARAM_DEFINE PARAM_RAZORING_ALPHA = 16;
+// [PARAM] min:4,max:32,step:1,interval:1,time_rate:1
+PARAM_DEFINE PARAM_RAZORING_ALPHA = 15;
 
 //
 // etc..
@@ -118,6 +118,20 @@ PARAM_DEFINE PARAM_QUIET_SEARCH_COUNT = 64;
 ここに過去の変更履歴が自動的に書き込まれる。
 右側にある「←」は値を減らしたときの勝率。「→」は値を増やしたときの勝率。
 [HISTORY]
+PARAM_FUTILITY_AT_PARENT_NODE_SEE_DEPTH : 4 → 4(50.00%) : ←←(値2,勝率50.31%,1728局,有意41.76%) ,←(値3,勝率51.15%,1728局,有意21.94%) ,→(値5,勝率51.30%,3456局,有意14.24%) ,→→(値6,勝率44.60%,576局,有意99.55%) ,
+PARAM_FUTILITY_AT_PARENT_NODE_DEPTH : 7 → 7(50.00%) : ←←(値5,勝率49.44%,3456局,有意67.74%) ,←(値6,勝率49.42%,1728局,有意65.07%) ,→(値8,勝率50.31%,1728局,有意41.78%) ,→→(値9,勝率49.85%,3456局,有意54.84%) ,
+PARAM_FUTILITY_RETURN_DEPTH : 7 → 7(50.00%) : ←←(値5,勝率48.91%,3456局,有意81.59%) ,←(値6,勝率47.50%,3456局,有意98.03%) ,→(値8,勝率50.26%,1728局,有意42.94%) ,→→(値9,勝率50.31%,1728局,有意41.76%) ,
+PARAM_RAZORING_ALPHA : 16 → 16(50.00%) : ←←(値12,勝率49.50%,3456局,有意66.01%) ,←(値14,勝率50.98%,1728局,有意25.62%) ,→(値18,勝率49.87%,1728局,有意53.56%) ,→→(値20,勝率50.44%,3456局,有意35.76%) ,
+PARAM_RAZORING_MARGIN : 512 → 512(50.00%) : ←←(値496,勝率46.30%,0局,有意96.44%) ,←(値504,勝率48.76%,1728局,有意79.76%) ,→(値520,勝率45.52%,0局,有意98.54%) ,→→(値528,勝率50.98%,3456局,有意21.10%) ,
+
+PARAM_REDUCTION_BY_HISTORY : 14980 → 14980(50.00%) : ← 48.94%(14724) in 1728 tries , 有意確率 = 76.20% , → 50.04%(15236) in 1728 tries , 有意確率 = 48.81%
+PARAM_SINGULAR_MARGIN : 8 → 8(50.00%) : ← 49.91%(6) in 1728 tries , 有意確率 = 52.37% , → 50.27%(10) in 1728 tries , 有意確率 = 42.92%
+PARAM_NULL_MOVE_DYNAMIC_BETA : 67 → 67(50.00%) : ← 48.77%(63) in 1728 tries , 有意確率 = 79.67% , → 51.09%(71) in 3456 tries , 有意確率 = 18.45%
+PARAM_NULL_MOVE_DYNAMIC_ALPHA : 823 → 823(50.00%) : ← 50.77%(807) in 3456 tries , 有意確率 = 26.34% , → 50.40%(839) in 1728 tries , 有意確率 = 39.41%
+PARAM_FUTILITY_AT_PARENT_NODE_MARGIN : 170 → 170(50.00%) : ← 49.38%(160) in 1728 tries , 有意確率 = 66.16% , → 50.22%(180) in 1728 tries , 有意確率 = 44.08%
+
+PARAM_FUTILITY_MARGIN_QUIET : 128 → 133(50.46%) : ← 49.00%(123) in 3456 tries  , → 50.46%(133) in 3456 tries
+PARAM_FUTILITY_MARGIN_ALPHA : 90 → 85(50.60%) : ← 50.60%(85) in 3456 tries  , → 49.09%(95) in 3456 tries
 
 */
 
