81e8f997aa0f1f9e3a4375516872e82ea738d145
yaneruao <yaneurao@gmail.com>
2017-05-18 19:25:57 +0900

    PieceNoをu8に変更。

--- source/shogi.h
+++ source/shogi.h
@@ -514,7 +514,8 @@ std::ostream& operator<<(std::ostream& os, Piece pc);
 // --------------------
 
 // Positionクラスで用いる、駒リスト(どの駒がどこにあるのか)を管理するときの番号。
-enum PieceNo {
+enum PieceNo : u8
+{
   PIECE_NO_PAWN = 0, PIECE_NO_LANCE = 18, PIECE_NO_KNIGHT = 22, PIECE_NO_SILVER = 26,
   PIECE_NO_GOLD = 30, PIECE_NO_BISHOP = 34, PIECE_NO_ROOK = 36, PIECE_NO_KING = 38, 
   PIECE_NO_BKING = 38, PIECE_NO_WKING = 39, // 先手、後手の玉の番号が必要な場合はこっちを用いる
@@ -522,7 +523,7 @@ enum PieceNo {
 };
 
 // PieceNoの整合性の検査。assert用。
-constexpr bool is_ok(PieceNo pn) { return PIECE_NO_ZERO <= pn && pn < PIECE_NO_NB; }
+constexpr bool is_ok(PieceNo pn) { return pn < PIECE_NO_NB; }
 
 // --------------------
 //       指し手
