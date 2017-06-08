3b731cfa46c8a98a24708c2fdaaf62d98145c946
yaneruao <yaneurao@gmail.com>
2017-06-08 23:07:44 +0900

    tt.h / tt.cpp reformatting

--- source/tt.cpp
+++ source/tt.cpp
@@ -5,27 +5,27 @@ TranspositionTable TT; // 置換表をglobalに確保。
 // 置換表のサイズを確保しなおす。
 void TranspositionTable::resize(size_t mbSize) {
 
-  size_t newClusterCount = size_t(1) << MSB64((mbSize * 1024 * 1024) / sizeof(Cluster));
+	size_t newClusterCount = size_t(1) << MSB64((mbSize * 1024 * 1024) / sizeof(Cluster));
 
-  // 同じサイズなら確保しなおす必要はない。
-  if (newClusterCount == clusterCount)
-    return;
+	// 同じサイズなら確保しなおす必要はない。
+	if (newClusterCount == clusterCount)
+		return;
 
-  clusterCount = newClusterCount;
+	clusterCount = newClusterCount;
 
-  free(mem);
+	free(mem);
 
-  // tableはCacheLineSizeでalignされたメモリに配置したいので、CacheLineSize-1だけ余分に確保する。
-  mem = calloc(clusterCount * sizeof(Cluster) + CacheLineSize - 1, 1);
+	// tableはCacheLineSizeでalignされたメモリに配置したいので、CacheLineSize-1だけ余分に確保する。
+	mem = calloc(clusterCount * sizeof(Cluster) + CacheLineSize - 1, 1);
 
-  if (!mem)
-  {
-    std::cout << "info string Error : Failed to allocate " << mbSize
-      << "MB for transposition table. ClusterCount = " << newClusterCount << std::endl;
-    exit(EXIT_FAILURE);
-  }
+	if (!mem)
+	{
+		std::cout << "info string Error : Failed to allocate " << mbSize
+			<< "MB for transposition table. ClusterCount = " << newClusterCount << std::endl;
+		exit(EXIT_FAILURE);
+	}
 
-  table = (Cluster*)((uintptr_t(mem) + CacheLineSize - 1) & ~(CacheLineSize - 1));
+	table = (Cluster*)((uintptr_t(mem) + CacheLineSize - 1) & ~(CacheLineSize - 1));
 }
 
 
@@ -55,9 +55,15 @@ TTEntry* TranspositionTable::probe(const Key key, bool& found) const
 		// returnする条件
 		// 1. keyが合致しているentryを見つけた。(found==trueにしてそのTT_ENTRYのアドレスを返す)
 		// 2. 空のエントリーを見つけた(そこまではkeyが合致していないので、found==falseにして新規TT_ENTRYのアドレスとして返す)
-		if (!tte[i].key16)
-			return found = false, &tte[i];      // このケースにおいてrefreshは必要ない。save()のときにgenerationを書き出すため。
 
+		// Stockfishのコードだと、1.が成立したタイミングでもgenerationのrefleshをしているが、
+		// save()のときにgenerationを書き出すため、このケースにおいてrefreshは必要ない。
+
+		// 1.
+		if (!tte[i].key16)
+			return found = false, &tte[i];
+
+		// 2.
 		if (tte[i].key16 == key16)
 		{
 			tte[i].set_generation(generation8); // Refresh
@@ -77,7 +83,7 @@ TTEntry* TranspositionTable::probe(const Key key, bool& found) const
 		// 以上の合計が一番小さいTTEntryを使う。
 
 		if (replace->depth8 - ((259 + generation8 - replace->genBound8) & 0xFC) * 2
-		  >   tte[i].depth8 - ((259 + generation8 - tte[i].genBound8) & 0xFC) * 2)
+		  >   tte[i].depth8 - ((259 + generation8 -   tte[i].genBound8) & 0xFC) * 2)
 			replace = &tte[i];
 
 	// generationは256になるとオーバーフローして0になるのでそれをうまく処理できなければならない。
@@ -93,15 +99,15 @@ TTEntry* TranspositionTable::probe(const Key key, bool& found) const
 
 int TranspositionTable::hashfull() const
 {
-  // すべてのエントリーにアクセスすると時間が非常にかかるため、先頭から1000エントリーだけ
-  // サンプリングして使用されているエントリー数を返す。
-  int cnt = 0;
-  for (int i = 0; i < 1000 / ClusterSize; ++i)
-  {
-    const auto tte = &table[i].entry[0];
-    for (int j = 0; j < ClusterSize; ++j)
-      if ((tte[j].generation() == generation8))
-        ++cnt;
-  }
-  return cnt;
+	// すべてのエントリーにアクセスすると時間が非常にかかるため、先頭から1000エントリーだけ
+	// サンプリングして使用されているエントリー数を返す。
+	int cnt = 0;
+	for (int i = 0; i < 1000 / ClusterSize; ++i)
+	{
+		const auto tte = &table[i].entry[0];
+		for (int j = 0; j < ClusterSize; ++j)
+			if ((tte[j].generation() == generation8))
+				++cnt;
+	}
+	return cnt;
 }
