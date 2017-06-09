e8ff7ba7e346f00309ad704154295bab1b31897e
yaneruao <yaneurao@gmail.com>
2017-06-09 23:36:53 +0900

    script/analyze_learning_log.pyの出力グラフ、test loss / learning lossのcross entropyの表示だけに変更。

--- script/analyze_learning_log.py
+++ script/analyze_learning_log.py
@@ -13,7 +13,7 @@ import pandas as pd
 def analyze_log(file_path):
     with open(file_path, 'rb') as fi:
         sfens_pat = re.compile(r'^(?P<sfens>\d+) sfens ,')
-        record_pat = re.compile(r'^hirate eval = (?P<hirate_eval>.*) , dsig rmse = (?P<dsig_rmse>.*) , dsig mae = (?P<dsig_mae>.*) , eval mae = (?P<eval_mae>.*) , cross_entropy_eval = (?P<cee>.*) , cross_entropy_win = (?P<cew>.*)')
+        record_pat = re.compile(r'^hirate eval = (?P<hirate_eval>.*) , dsig rmse = (?P<dsig_rmse>.*) , dsig mae = (?P<dsig_mae>.*) , eval mae = (?P<eval_mae>.*) , test_cross_entropy_eval = (?P<tcee>.*) , test_cross_entropy_win = (?P<tcew>.*) , test_cross_entropy = (?P<tce>.*) , learn_cross_entropy_eval = (?P<lcee>.*) , learn_cross_entropy_win = (?P<lcew>.*) , learn_cross_entropy = (?P<lce>.*)')
         log = []
         for line in fi.readlines():
             mo = sfens_pat.search(line)
@@ -24,15 +24,16 @@ def analyze_log(file_path):
             mo = record_pat.search(line)
             if mo:
                 sfens += 1000000     # output every 1M sfens.
+#                sfens += 200000     # output every 200k sfens.
                 if sfens < 10000000: # skip early period
                     continue;
                 hirate_eval = float(mo.groupdict()['hirate_eval'])
                 dsig_rmse    = float(mo.groupdict()['dsig_rmse'])
                 dsig_mae    = float(mo.groupdict()['dsig_mae'])
                 eval_mae    = float(mo.groupdict()['eval_mae'])
-                cee         = float(mo.groupdict()['cee'])
-                cew         = float(mo.groupdict()['cew'])
-                log.append((sfens, hirate_eval, dsig_rmse , dsig_mae , eval_mae , cee , cew))
+                tce         = float(mo.groupdict()['tce'])
+                lce         = float(mo.groupdict()['lce'])
+                log.append((sfens, hirate_eval, dsig_rmse , dsig_mae , eval_mae , tce , lce))
 
     if len(log) == 0:
         print('{}: Empty'.format(file_path))
@@ -41,35 +42,35 @@ def analyze_log(file_path):
         print('{}: {}'.format(file_path, len(log)))
 
     # dataframe
-    df = pd.DataFrame(data=log, columns='sfens hirate_eval dsig_rmse dsig_mae eval_mae cee cew'.split())
+    df = pd.DataFrame(data=log, columns='sfens hirate_eval dsig_rmse dsig_mae eval_mae tce lce'.split())
 
     # plot
     fig, ax = plt.subplots(1, 1)
-    ax.plot(
-            df['sfens'],
-            df['dsig_rmse'],
-            color='blue', label='RMSE')
-    ax.set_xlabel('# SFENs')
-    ax.set_ylabel('DSIG_RMSE')
-    ax.legend(loc='upper left').get_frame().set_alpha(0.5)
+#    ax.plot(
+#            df['sfens'],
+#            df['dsig_rmse'],
+#            color='blue', label='RMSE')
+#    ax.set_xlabel('# SFENs')
+#    ax.set_ylabel('DSIG_RMSE')
+#    ax.legend(loc='upper left').get_frame().set_alpha(0.5)
 
     ax = ax.twinx()
     ax.plot(
             df['sfens'],
-            df['cee'],
-		    color='red', label='cee')
+            df['tce'],
+		    color='red', label='tce')
     ax.set_xlabel('# SFENs')
-    ax.set_ylabel('cee')
+    ax.set_ylabel('tce')
     ax.legend(loc='upper right').get_frame().set_alpha(0.5)
 
     ax = ax.twinx()
     ax.plot(
             df['sfens'],
-            df['cew'],
-		    color='green', label='cew')
+            df['lce'],
+		    color='green', label='lce')
     ax.set_xlabel('# SFENs')
-    ax.set_ylabel('cew')
-    ax.legend(loc='lower right').get_frame().set_alpha(0.5)
+    ax.set_ylabel('lce')
+    ax.legend(loc='upper left').get_frame().set_alpha(0.5)
 
     ax.set_title(file_path)
 
