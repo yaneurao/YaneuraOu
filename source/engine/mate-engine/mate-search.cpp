cf9527c79fe98783ae4e5d3ae36a757bc3389271
Hisayori Noda <nodchip@gmail.com>
2018-04-15 00:27:55 +0900

    対局規定第8条第7項に従って修正した

--- source/engine/mate-engine/mate-search.cpp
+++ source/engine/mate-engine/mate-search.cpp
@@ -253,36 +253,54 @@ namespace MateEngine
 			return;
 		}
 
-		auto draw_type = n.is_repetition(n.game_ply());
-		switch (draw_type) {
-		case REPETITION_WIN:
-			if (or_node) {
-				// ここは通らないはず
-				entry.pn = 0;
-				entry.dn = kInfinitePnDn;
-				entry.minimum_distance = std::min(entry.minimum_distance, depth);
-			}
-			else {
-				entry.pn = kInfinitePnDn;
-				entry.dn = 0;
-				entry.minimum_distance = std::min(entry.minimum_distance, depth);
-			}
-			return;
-		case REPETITION_LOSE:
-		case REPETITION_DRAW:
-			if (or_node) {
-				entry.pn = kInfinitePnDn;
-				entry.dn = 0;
-				entry.minimum_distance = std::min(entry.minimum_distance, depth);
-			}
-			else {
-				// ここは通らないはず
-				entry.pn = 0;
-				entry.dn = kInfinitePnDn;
-				entry.minimum_distance = std::min(entry.minimum_distance, depth);
-			}
-			return;
-		}
+    // 千日手のチェック
+    // 対局規定（抄録）｜よくある質問｜日本将棋連盟 https://www.shogi.or.jp/faq/taikyoku-kitei.html
+    // 第8条 反則
+    // 7. 連続王手の千日手とは、同一局面が４回出現した一連の手順中、片方の手が
+    // すべて王手だった場合を指し、王手を続けた側がその時点で負けとなる。
+    // 従って開始局面により、連続王手の千日手成立局面が王手をかけた状態と
+    // 王手を解除した状態の二つのケースがある。 （※）
+    // （※）は平成25年10月1日より暫定施行。
+    auto draw_type = n.is_repetition(n.game_ply());
+    switch (draw_type) {
+    case REPETITION_WIN:
+      // 連続王手の千日手による勝ち
+      if (or_node) {
+        // ここは通らないはず
+        entry.pn = 0;
+        entry.dn = kInfinitePnDn;
+        entry.minimum_distance = std::min(entry.minimum_distance, depth);
+      }
+      else {
+        entry.pn = kInfinitePnDn;
+        entry.dn = 0;
+        entry.minimum_distance = std::min(entry.minimum_distance, depth);
+      }
+      return;
+
+    case REPETITION_LOSE:
+      // 連続王手の千日手による負け
+      if (or_node) {
+        entry.pn = kInfinitePnDn;
+        entry.dn = 0;
+        entry.minimum_distance = std::min(entry.minimum_distance, depth);
+      }
+      else {
+        // ここは通らないはず
+        entry.pn = 0;
+        entry.dn = kInfinitePnDn;
+        entry.minimum_distance = std::min(entry.minimum_distance, depth);
+      }
+      return;
+
+    case REPETITION_DRAW:
+      // 普通の千日手
+      // ここは通らないはず
+      entry.pn = kInfinitePnDn;
+      entry.dn = 0;
+      entry.minimum_distance = std::min(entry.minimum_distance, depth);
+      return;
+    }
 
 		MovePicker move_picker(n, or_node);
 		if (move_picker.empty()) {
