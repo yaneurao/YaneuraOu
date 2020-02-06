97082f0c03935bdac50819b4e4664f0347d9287d
yaneruao <yaneurao@gmail.com>
2020-02-06 14:15:14 +0900

    - NNUEの学習用のビルドで、gccで未初期化の配列がある警告が出ていたのを抑制。

--- source/eval/nnue/features/index_list.h
+++ source/eval/nnue/features/index_list.h
@@ -19,26 +19,26 @@ namespace Features {
 // 特徴量のインデックスリストに使うクラステンプレート
 template <typename T, std::size_t MaxSize>
 class ValueList {
- public:
-  std::size_t size() const { return size_; }
-  void resize(std::size_t size) { size_ = size; }
-  void push_back(const T& value) { values_[size_++] = value; }
-  T& operator[](std::size_t index) { return values_[index]; }
-  T* begin() { return values_; }
-  T* end() { return values_ + size_; }
-  const T& operator[](std::size_t index) const { return values_[index]; }
-  const T* begin() const { return values_; }
-  const T* end() const { return values_ + size_; }
-  void swap(ValueList& other) {
-    const std::size_t max_size = std::max(size_, other.size_);
-    for (std::size_t i = 0; i < max_size; ++i) {
-      std::swap(values_[i], other.values_[i]);
+public:
+    std::size_t size() const { return size_; }
+    void resize(std::size_t size) { size_ = size; }
+    void push_back(const T& value) { values_[size_++] = value; }
+    T& operator[](std::size_t index) { return values_[index]; }
+    T* begin() { return values_; }
+    T* end() { return values_ + size_; }
+    const T& operator[](std::size_t index) const { return values_[index]; }
+    const T* begin() const { return values_; }
+    const T* end() const { return values_ + size_; }
+    void swap(ValueList& other) {
+        const std::size_t max_size = std::max(size_, other.size_);
+        for (std::size_t i = 0; i < max_size; ++i) {
+          std::swap(values_[i], other.values_[i]);
+        }
+        std::swap(size_, other.size_);
     }
-    std::swap(size_, other.size_);
-  }
- private:
-  T values_[MaxSize];
-  std::size_t size_ = 0;
+private:
+    T values_[MaxSize] = {};
+    std::size_t size_ = 0;
 };
 
 // 特徴量のインデックスリストの型
