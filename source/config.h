276faf80d51dd6cae053112db8021171d5dbf4e8
yaneruao <yaneurao@gmail.com>
2020-08-01 02:48:40 +0900

    - Ryzen向け最適化
    	- Apery型の遠方駒の利きを求めるコードをporting。
    
    	- ZEN/ZEN2(Ryzen 第一世代、第二世代)のPEXTがμOPレベルでのemulationだから遅い。
    	- ZEN/ZEN2向けに、PEXT使ってるところ、magic bitboard導入する。
    		- Aperyのmagic bitboard実装をそのまま持ってくる。
    		//  
    https://github.com/HiraokaTakuya/apery/blob/master/src/bitboard.cpp
    
    	- やねうら王では、PEXT命令、飛車の利きなどで使っている。
    		このsoftware emulationのコード、1になっているbitの数は64bitのうちの8個程度(平均的には5個程度？)であり、
    		1のbitの回数だけループを回すようなアルゴリズムで、テーブルも参照しないコードなので、わりと速い気はする。
    		やねうら王の状況では、やねうら王のsoftware emulationのコードのほうが断然速い。(実測で6%程度速いらしい)
    
    	- magic bitboardにするとここがさらに速くなる。
    		- ZEN/ZEN2向けはPEXTを完全に排除したので、V4.91から10%以上高速化しているはず。
    
    	- AVX2(非ZEN/ZEN2)に対しても、今回実装した、新しい利きのコードを用いる。
    		テーブルサイズは少し大きくなるが、速度はほぼ変わらないので。
    
    	- 55将棋のようなものを作るときに、magic bitboardのmagic numberを求めるの、ちょっと嫌なので
    		従来の実装は残しておく。
    			USE_MAGIC_BITBOARDをdefineしたときだけ、今回追加されたApery型の遠方駒の利きを求めるコードを
    			用いるようにする。

--- source/config.h
+++ source/config.h
@@ -56,15 +56,26 @@
 #define USE_AVX2
 //#define USE_SSE42
 //#define USE_SSE41
-//#define USE_SSE3
+//#define USE_SSSE3
 //#define USE_SSE2
 //#define NO_SSE
 
+// -- BMI2命令を使う/使わない
+
+// AVX2環境でBMI2が使えるときに、BMI2対応命令を使うのか(ZEN/ZEN2ではBMI2命令は使わないほうが速い)
+// AMDのRyzenシリーズ(ZEN1/ZEN2)では、BMI2命令遅いので、これを定義しないほうが速い。
+#define USE_BMI2
+
+// やねうら王の従来の遠方駒の利きを求めるコードを用いる。
+// これをundefするとApery型の利きのコードを用いる。(そっちのほうがPEXTが使えない環境だと速い)
+// 互換性維持および、55将棋のように盤面を変形させるときに、magic tableで用いるmagic numberを求めたくないときに用いる。
+
+// #define USE_OLD_YANEURAOU_EFFECT
+
+
 #else
 
-// Makefileを使ってbuildするときは、
-// $ make avx2
-// のようにしてビルドすれば自動的にAVX2用実行ファイルがビルドされます。
+// Makefileを使ってbuildする手順は docs/解説.txt を見ること。
 
 #endif
 
@@ -250,6 +261,8 @@
 // PVの出力時の千日手に関する出力をすべて"rep_draw"に変更するオプション。
 // GUI側が、何らかの都合で"rep_draw"のみしか処理できないときに用いる。
 // #define PV_OUTPUT_DRAW_ONLY
+
+
 
 // --------------------
 // release configurations
@@ -510,16 +523,22 @@ constexpr bool Is64Bit = true;
 constexpr bool Is64Bit = false;
 #endif
 
+#if defined(USE_BMI2)
+#define BMI2_STR "BMI2"
+#else
+#define BMI2_STR ""
+#endif
+
 #if defined(USE_AVX512)
-#define TARGET_CPU "AVX512"
+#define TARGET_CPU "AVX512" BMI2_STR
 #elif defined(USE_AVX2)
-#define TARGET_CPU "AVX2"
+#define TARGET_CPU "AVX2" BMI2_STR
 #elif defined(USE_SSE42)
 #define TARGET_CPU "SSE4.2"
 #elif defined(USE_SSE41)
 #define TARGET_CPU "SSE4.1"
-#elif defined(USE_SSE3)
-#define TARGET_CPU "SSE3"
+#elif defined(USE_SSSE3)
+#define TARGET_CPU "SSSE3"
 #elif defined(USE_SSE2)
 #define TARGET_CPU "SSE2"
 #else
