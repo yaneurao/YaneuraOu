5ea7df0533b5457268c7e94408f00c479000c147
nodchip <nodchip@gmail.com>
2021-10-28 18:08:25 +0900

    HalfKP_vmを追加した
    # Conflicts:
    #	source/YaneuraOu.vcxproj.filters

---
+++ source/eval/nnue/features/half_kp_vm.cpp
@@ -0,0 +1,88 @@
+﻿// NNUE評価関数の入力特徴量HalfKP_vmの定義
+
+#include "../../../config.h"
+
+#if defined(EVAL_NNUE)
+
+#include "half_kp_vm.h"
+#include "index_list.h"
+
+namespace Eval {
+
+	namespace NNUE {
+
+		namespace Features {
+
+			// 玉の位置とBonaPieceから特徴量のインデックスを求める
+			template <Side AssociatedKing>
+			inline IndexType HalfKP_vm<AssociatedKing>::MakeIndex(Square sq_k, BonaPiece p) {
+				if (sq_k >= SQ_61) {
+					sq_k = Mir(sq_k);
+
+					IndexType piece_index = (p - fe_hand_end) / SQ_NB;
+					Square sq_p = static_cast<Square>((p - fe_hand_end) % SQ_NB);
+					sq_p = Mir(sq_p);
+					p = static_cast<BonaPiece>(fe_hand_end + piece_index * static_cast<IndexType>(SQ_NB) + sq_p);
+				}
+				return static_cast<IndexType>(fe_end) * static_cast<IndexType>(sq_k) + p;
+			}
+
+			// 駒の情報を取得する
+			template <Side AssociatedKing>
+			inline void HalfKP_vm<AssociatedKing>::GetPieces(
+				const Position& pos, Color perspective,
+				BonaPiece** pieces, Square* sq_target_k) {
+				*pieces = (perspective == BLACK) ?
+					pos.eval_list()->piece_list_fb() :
+					pos.eval_list()->piece_list_fw();
+				const PieceNumber target = (AssociatedKing == Side::kFriend) ?
+					static_cast<PieceNumber>(PIECE_NUMBER_KING + perspective) :
+					static_cast<PieceNumber>(PIECE_NUMBER_KING + ~perspective);
+				*sq_target_k = static_cast<Square>(((*pieces)[target] - f_king) % SQ_NB);
+			}
+
+			// 特徴量のうち、値が1であるインデックスのリストを取得する
+			template <Side AssociatedKing>
+			void HalfKP_vm<AssociatedKing>::AppendActiveIndices(
+				const Position& pos, Color perspective, IndexList* active) {
+				// コンパイラの警告を回避するため、配列サイズが小さい場合は何もしない
+				if (RawFeatures::kMaxActiveDimensions < kMaxActiveDimensions) return;
+
+				BonaPiece* pieces;
+				Square sq_target_k;
+				GetPieces(pos, perspective, &pieces, &sq_target_k);
+				for (PieceNumber i = PIECE_NUMBER_ZERO; i < PIECE_NUMBER_KING; ++i) {
+					active->push_back(MakeIndex(sq_target_k, pieces[i]));
+				}
+			}
+
+			// 特徴量のうち、一手前から値が変化したインデックスのリストを取得する
+			template <Side AssociatedKing>
+			void HalfKP_vm<AssociatedKing>::AppendChangedIndices(
+				const Position& pos, Color perspective,
+				IndexList* removed, IndexList* added) {
+				BonaPiece* pieces;
+				Square sq_target_k;
+				GetPieces(pos, perspective, &pieces, &sq_target_k);
+				const auto& dp = pos.state()->dirtyPiece;
+				for (int i = 0; i < dp.dirty_num; ++i) {
+					if (dp.pieceNo[i] >= PIECE_NUMBER_KING) continue;
+					const auto old_p = static_cast<BonaPiece>(
+						dp.changed_piece[i].old_piece.from[perspective]);
+					removed->push_back(MakeIndex(sq_target_k, old_p));
+					const auto new_p = static_cast<BonaPiece>(
+						dp.changed_piece[i].new_piece.from[perspective]);
+					added->push_back(MakeIndex(sq_target_k, new_p));
+				}
+			}
+
+			template class HalfKP_vm<Side::kFriend>;
+			template class HalfKP_vm<Side::kEnemy>;
+
+		}  // namespace Features
+
+	}  // namespace NNUE
+
+}  // namespace Eval
+
+#endif  // defined(EVAL_NNUE)
---
+++ source/eval/nnue/features/half_kp_vm.h
@@ -0,0 +1,65 @@
+﻿// NNUE評価関数の入力特徴量HalfKP_vmの定義
+
+#ifndef _NNUE_FEATURES_HALF_KP_VM_H_
+#define _NNUE_FEATURES_HALF_KP_VM_H_
+
+#include "../../../config.h"
+
+#if defined(EVAL_NNUE)
+
+#include "../../../evaluate.h"
+#include "features_common.h"
+
+namespace Eval {
+
+	namespace NNUE {
+
+		namespace Features {
+
+			// 特徴量HalfKP_vm：自玉または敵玉の位置と、玉以外の駒の位置の組み合わせ
+			// 6筋～9筋に玉がいる場合、4筋～1筋に反転させる
+			template <Side AssociatedKing>
+			class HalfKP_vm {
+			public:
+				// 特徴量名
+				static constexpr const char* kName =
+					(AssociatedKing == Side::kFriend) ? "HalfKP_vm(Friend)" : "HalfKP_vm(Enemy)";
+				// 評価関数ファイルに埋め込むハッシュ値
+				static constexpr std::uint32_t kHashValue =
+					0x0B6B1D9Bu ^ (AssociatedKing == Side::kFriend);
+				// 特徴量の次元数
+				static constexpr IndexType kDimensions =
+					5 * static_cast<IndexType>(FILE_NB) * static_cast<IndexType>(fe_end);
+				// 特徴量のうち、同時に値が1となるインデックスの数の最大値
+				static constexpr IndexType kMaxActiveDimensions = PIECE_NUMBER_KING;
+				// 差分計算の代わりに全計算を行うタイミング
+				static constexpr TriggerEvent kRefreshTrigger =
+					(AssociatedKing == Side::kFriend) ?
+					TriggerEvent::kFriendKingMoved : TriggerEvent::kEnemyKingMoved;
+
+				// 特徴量のうち、値が1であるインデックスのリストを取得する
+				static void AppendActiveIndices(const Position& pos, Color perspective,
+					IndexList* active);
+
+				// 特徴量のうち、一手前から値が変化したインデックスのリストを取得する
+				static void AppendChangedIndices(const Position& pos, Color perspective,
+					IndexList* removed, IndexList* added);
+
+				// 玉の位置とBonaPieceから特徴量のインデックスを求める
+				static IndexType MakeIndex(Square sq_k, BonaPiece p);
+
+			private:
+				// 駒の情報を取得する
+				static void GetPieces(const Position& pos, Color perspective,
+					BonaPiece** pieces, Square* sq_target_k);
+			};
+
+		}  // namespace Features
+
+	}  // namespace NNUE
+
+}  // namespace Eval
+
+#endif  // defined(EVAL_NNUE)
+
+#endif
