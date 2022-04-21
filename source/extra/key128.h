1ce3d279f63c775449b6332597f57666c5fad535
yaneurao <yaneurao@gmail.com>
2022-04-22 02:34:19 +0900

    - Key128、Key256、AVX2以外の環境で動くようにするの忘れていたの修正。

--- source/extra/key128.h
+++ source/extra/key128.h
@@ -21,36 +21,40 @@
 // 64bit版
 typedef uint64_t Key64;
 
-// 実験用の機能なので、128bit,256bitのhash keyのサポートはAVX2のみ。
-#if defined (USE_AVX2)
-
 // 128bit版
 struct alignas(16) Key128
 {
+#if defined (USE_SSE2)
 	union {
 		__m128i m;
-		u64 _u64[2];
+		u64 p[2];
 	};
+#else
+		u64 p[2];
+#endif
 
 	Key128() {}
 
 	// kを下位64bitに格納する(上位64bitは0)
 	Key128(const Key& k) { set(k,0); }
+
+#if defined (USE_SSE2)
 	Key128(const Key128& bb) { _mm_store_si128(&this->m, bb.m); }
 	Key128& operator = (const Key128& rhs) { _mm_store_si128(&this->m, rhs.m); return *this; }
+#else
+	// 定義不要？
+#endif
 
 	// 下位64bitをk0、上位64bitをk1にする。
 	void set(Key k0, Key k1) {
 #if defined(USE_SSE2)
 	m = _mm_set_epi64x(k1,k0);
 #else
-	_u64[0] = k0; _u64[1] = k1;
+	p[0] = k0; p[1] = k1;
 #endif
 	}
 
-	operator Key() const { return _u64[0]; }
-
-	uint64_t p(int i) const { return _u64[i]; }
+	operator Key() const { return extract64<0>(); }
 
 	// _u64[n]を取り出す。SSE4の命令が使えるときはそれを使う。
 	// n == 0なら下位64bit、n == 1なら上位64bitが取り出される。
@@ -61,17 +65,27 @@ struct alignas(16) Key128
 	#if defined(USE_SSE41)
 		return (u64)(_mm_extract_epi64(m, n));
 	#else
-		return _u64[n];
+		return p[n];
 	#endif
 	}
 
+#if defined (USE_SSE2)
 	Key128& operator += (const Key128& b1) { this->m = _mm_add_epi64(m, b1.m); return *this; }
 	Key128& operator -= (const Key128& b1) { this->m = _mm_sub_epi64(m, b1.m); return *this; }
 	Key128& operator ^= (const Key128& b1) { this->m = _mm_xor_si128(m, b1.m); return *this; }
+#else
+	Key128& operator += (const Key128& b1) { this->p[0] += b1.p[0]; this->p[1] += b1.p[1]; return *this; }
+	Key128& operator -= (const Key128& b1) { this->p[0] -= b1.p[0]; this->p[1] -= b1.p[1]; return *this; }
+	Key128& operator ^= (const Key128& b1) { this->p[0] ^= b1.p[0]; this->p[1] ^= b1.p[1]; return *this; }
+#endif
 
-	Key128& operator *= (int64_t i) { _u64[0] *= i; _u64[1] *= i; return *this; }
+	Key128& operator *= (int64_t i) { p[0] *= i; p[1] *= i; return *this; }
 	bool operator == (const Key128& rhs) const {
+#if defined (USE_SSE2)
 		return (_mm_testc_si128(_mm_cmpeq_epi8(this->m, rhs.m), _mm_set1_epi8(static_cast<char>(0xffu))) ? true : false);
+#else
+		return p[0]==rhs.p[0] && p[1]==rhs.p[1];
+#endif
 	}
 	bool operator != (const Key128& rhs) const { return !(*this == rhs); }
 
@@ -87,56 +101,70 @@ template <>
 struct std::hash<Key128> {
 	size_t operator()(const Key128& k) const {
 		// 下位bit返すだけで良いのでは？
-		return (size_t)(k._u64[0]);
+		return (size_t)(k.extract64<0>());
 	}
 };
 
 // 256bit版
 struct alignas(32) Key256
 {
+#if defined(USE_AVX2)
 	union {
 		__m256i m;
-		u64 _u64[4];
+		u64 p[4];
 	};
+#else
+		u64 p[4];
+#endif
 
 	Key256() {}
 	Key256(const Key& k) { set(k, 0, 0, 0); }
+#if defined(USE_AVX2)
 	Key256(const Key256& bb) { _mm256_store_si256(&this->m, bb.m); }
 	Key256& operator = (const Key256& rhs) { _mm256_store_si256(&this->m, rhs.m); return *this; }
+#endif
 
 	// 下位64bitから順にk0, k1, k2, k3に設定する。
 	void set(Key k0, Key k1, Key k2, Key k3) {
-#if defined(USE_SSE2)
+#if defined(USE_AVX2)
 	m = _mm256_set_epi64x(k3,k2,k1,k0);
 #else
-	_u64[0] = k0; _u64[1] = k1; _u64[2] = k2; _u64[3] = k3;
+	p[0] = k0; p[1] = k1; p[2] = k2; p[3] = k3;
 #endif
 	}
 
-	operator Key() const { return _u64[0]; }
-
-	uint64_t p(int i) const { return _u64[i]; }
+	operator Key() const { return extract64<0>(); }
 
 	// p[n]を取り出す。SSE4の命令が使えるときはそれを使う。
 	template <int n>
 	u64 extract64() const
 	{
 		static_assert(n == 0 || n == 1 || n == 2 || n == 3 , "");
-	#if defined(USE_SSE41)
+	#if defined(USE_AVX2)
 		return (u64)(_mm256_extract_epi64(m, n));
 	#else
-		return _u64[n];
+		return p[n];
 	#endif
 	}
 
+#if defined(USE_AVX2)
 	Key256& operator += (const Key256& b1) { this->m = _mm256_add_epi64(m, b1.m); return *this; }
 	Key256& operator -= (const Key256& b1) { this->m = _mm256_sub_epi64(m, b1.m); return *this; }
 	Key256& operator ^= (const Key256& b1) { this->m = _mm256_xor_si256(m, b1.m); return *this; }
-
-	Key256& operator *= (int64_t i) { _u64[0] *= i; _u64[1] *= i; _u64[2] *= i; _u64[3] *= i; return *this; }
+#else
+	Key256& operator += (const Key256& b1) { this->p[0] += b1.p[0]; this->p[1] += b1.p[1]; this->p[2] += b1.p[2]; this->p[3] += b1.p[3]; return *this; }
+	Key256& operator -= (const Key256& b1) { this->p[0] -= b1.p[0]; this->p[1] -= b1.p[1]; this->p[2] -= b1.p[2]; this->p[3] -= b1.p[3]; return *this; }
+	Key256& operator ^= (const Key256& b1) { this->p[0] ^= b1.p[0]; this->p[1] ^= b1.p[1]; this->p[2] ^= b1.p[2]; this->p[3] ^= b1.p[3]; return *this; }
+#endif
+	Key256& operator *= (int64_t i) { p[0] *= i; p[1] *= i; p[2] *= i; p[3] *= i; return *this; }
 	bool operator == (const Key256& rhs) const {
+#if defined(USE_AVX2)
 		return (_mm256_testc_si256(_mm256_cmpeq_epi8(this->m, rhs.m), _mm256_set1_epi8(static_cast<char>(0xffu))) ? true : false);
+#else
+		return p[0]==rhs.p[0] && p[1]==rhs.p[1] && p[2]==rhs.p[2] && p[3]==rhs.p[3];
+#endif
 	}
+
 	bool operator != (const Key256& rhs) const { return !(*this == rhs); }
 
 	Key256 operator * (const int64_t i) const { return Key256(*this) *= i; }
@@ -150,10 +178,8 @@ template <>
 struct std::hash<Key256> {
 	size_t operator()(const Key256& k) const {
 		// 下位bit返すだけで良いのでは？
-		return (size_t)(k._u64[0]);
+		return (size_t)(k.extract64<0>());
 	}
 };
-
-#endif
 
 #endif // _KEY128_H_
