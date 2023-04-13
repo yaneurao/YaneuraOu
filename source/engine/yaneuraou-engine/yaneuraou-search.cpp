2bc1653bb486fa97fb22d65a4dd623e9b8af50a1
nodchip <nodchip@gmail.com>
2023-04-13 23:15:07 +0900

    Remove old line in "Futility pruning for captures" The line is no longer needed after 910cf8b. This patch incidentally applies "Futility Pruning for Captures" also in case of en-passant, changing the bench signature. https://github.com/official-stockfish/Stockfish/commit/d5271af0ee28cb52e1033f183f3b34754aa5efa0

--- source/engine/yaneuraou-engine/yaneuraou-search.cpp
+++ source/engine/yaneuraou-engine/yaneuraou-search.cpp
@@ -2230,8 +2230,7 @@ namespace {
 #endif
 
 					// Futility pruning for captures (~0 Elo)
-					if (   !pos.empty(to_sq(move))
-						&& !givesCheck
+					if (   !givesCheck
 						&& !PvNode
 						&& lmrDepth < 6
 						&& !ss->inCheck
