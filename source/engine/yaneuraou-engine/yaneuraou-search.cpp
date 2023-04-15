0932fd02c9de91320065c50c2cd4fcef1be17143
nodchip <nodchip@gmail.com>
2023-04-15 17:02:44 +0900

    Remove PvNode Parameter for cutoff LMR https://github.com/official-stockfish/Stockfish/commit/6a6faac04db26daae6a77b6df6529e22d549531b

--- source/engine/yaneuraou-engine/yaneuraou-search.cpp
+++ source/engine/yaneuraou-engine/yaneuraou-search.cpp
@@ -2526,8 +2526,8 @@ namespace {
 				if (singularQuietLMR)
 					r--;
 
-				// Increase reduction if next ply has a lot of fail high else reset count to 0
-				if ((ss + 1)->cutoffCnt > 3 && !PvNode)
+				// Increase reduction if next ply has a lot of fail high
+				if ((ss + 1)->cutoffCnt > 3)
 					r++;
 
 				// 【計測資料 11.】statScoreの計算でcontHist[3]も調べるかどうか。
