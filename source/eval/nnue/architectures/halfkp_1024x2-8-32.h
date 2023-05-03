f825c9f3b0145a0cec1445f860e2cfe45cb45c47
nodchip <nodchip@gmail.com>
2023-05-03 20:10:15 +0900

    halfkp_1024x2-8-32 halfkp_512x2-16-32 を追加した (#261)
    # Conflicts:
    #	source/Makefile
    #	source/YaneuraOu.vcxproj.filters
    
    # Conflicts:
    #	source/Makefile
    #	source/YaneuraOu.vcxproj
    #	source/eval/nnue/nnue_architecture.h
    
    # Conflicts:
    #	source/YaneuraOu.vcxproj.filters

---
+++ source/eval/nnue/architectures/halfkp_1024x2-8-32.h
@@ -0,0 +1,39 @@
+﻿// Definition of input features and network structure used in NNUE evaluation function
+// NNUE評価関数で用いる入力特徴量とネットワーク構造の定義
+#ifndef NNUE_HALFKP_1024X2_8_32_H_INCLUDED
+#define NNUE_HALFKP_1024X2_8_32_H_INCLUDED
+
+#include "../features/feature_set.h"
+#include "../features/half_kp.h"
+
+#include "../layers/input_slice.h"
+#include "../layers/affine_transform.h"
+#include "../layers/clipped_relu.h"
+
+namespace Eval::NNUE {
+
+// Input features used in evaluation function
+// 評価関数で用いる入力特徴量
+using RawFeatures = Features::FeatureSet<
+    Features::HalfKP<Features::Side::kFriend>>;
+
+// Number of input feature dimensions after conversion
+// 変換後の入力特徴量の次元数
+constexpr IndexType kTransformedFeatureDimensions = 1024;
+
+namespace Layers {
+
+// Define network structure
+// ネットワーク構造の定義
+using InputLayer = InputSlice<kTransformedFeatureDimensions * 2>;
+using HiddenLayer1 = ClippedReLU<AffineTransform<InputLayer, 8>>;
+using HiddenLayer2 = ClippedReLU<AffineTransform<HiddenLayer1, 32>>;
+using OutputLayer = AffineTransform<HiddenLayer2, 1>;
+
+}  // namespace Layers
+
+using Network = Layers::OutputLayer;
+
+}  // namespace Eval::NNUE
+
+#endif // #ifndef NNUE_HALFKP_1024X2_8_32_H_INCLUDED
