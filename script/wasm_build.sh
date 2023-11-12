95c9c9d794c1427d1ba7ffa0c49096b66207acea
WandererXII <43043181+WandererXII@users.noreply.github.com>
2023-11-12 03:11:19 +0100

    Use emsdk:3.1.43 not latest (#269)

--- script/wasm_build.sh
+++ script/wasm_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pushd `dirname $0`
 pushd ../
-docker pull emscripten/emsdk:latest
-docker run --rm -v ${PWD}:/src emscripten/emsdk:latest node script/wasm_build.js
+docker pull emscripten/emsdk:3.1.43
+docker run --rm -v ${PWD}:/src emscripten/emsdk:3.1.43 node script/wasm_build.js
 popd
 popd
