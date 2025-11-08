d72853b4fae5ede2399ef7aa006b3c887c6efd55
Lancali <6326211+Lancali@users.noreply.github.com>
2025-11-08 00:04:08 -0800

    Use pre-increment for fundamental types

--- source/tune.h
+++ source/tune.h
@@ -200,7 +200,7 @@ class Tune {
 
 	template<typename T, size_t N, typename... Args>
     int add(const SetRange& range, std::string&& names, T (&value)[N], Args&&... args) {
-        for (size_t i = 0; i < N; i++)
+        for (size_t i = 0; i < N; ++i)
             add(range, next(names, i == N - 1) + "[" + std::to_string(i) + "]", value[i]);
         return add(range, std::move(names), args...);
     }
