e4589706b973847b5db834bd449122a71d72b565
yaneurao <yaneurao@gmail.com>
2026-03-01 23:32:43 +0900

    - V9.21

--- source/config.h
+++ source/config.h
@@ -14,7 +14,7 @@
 // ただし、この値を数値として使用することがあるので数値化できる文字列にしておく必要がある。
 #if !defined(ENGINE_VERSION)
 
-#define ENGINE_VERSION "9.20git"
+#define ENGINE_VERSION "9.21git"
 
 #endif
 // --------------------
