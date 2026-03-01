5920281af45897e0387510f3263bc5cca6fbf9be
yaneurao <yaneurao@gmail.com>
2026-03-01 22:07:07 +0900

    - sfnnwop-1536、ビルド通らなくなっていたの修正。

--- source/eval/nnue/architectures/sfnnwop-1536.h
+++ source/eval/nnue/architectures/sfnnwop-1536.h
@@ -4,7 +4,7 @@
 #define CLASSIC_NNUE_SFNNWOP_1536_H_INCLUDED
 
 #include "../features/feature_set.h"
-#include "../features/half_ka_hm.h"
+#include "../features/half_ka_hm2.h"
 
 #include <cstring>
 
@@ -19,7 +19,7 @@ namespace Eval::NNUE {
 // Input features used in evaluation function
 // 評価関数で用いる入力特徴量
 using RawFeatures = Features::FeatureSet<
-    Features::HalfKA_hm<Features::Side::kFriend>>;
+    Features::HalfKA_hm2<Features::Side::kFriend>>;
 
 // Number of input feature dimensions after conversion
 // 変換後の入力特徴量の次元数
