12b416dac19c8156a38a476f11f08c70d133d89b
yaneurao <yaneurao@gmail.com>
2026-06-13 02:04:14 +0900

    - peta shock化コマンドのprogressがでなくなっていたのを修正。

--- source/book/makebook.cpp
+++ source/book/makebook.cpp
@@ -3,6 +3,7 @@
 #if defined (ENABLE_MAKEBOOK_CMD)
 
 #include "book.h"
+#include "../misc.h"
 #include "../position.h"
 #include <sstream>
 
@@ -29,18 +30,23 @@ namespace Book
         // このタイミングでisready()で初期化しておく。
         // (そうしないとPosition classやevalが使えなくて困る)
         engine.isready();
+		Tools::ProgressBar::enable(true);
 
 		string token;
 		is >> token;
 
 		// 2025年に作ったmakebook拡張コマンド
         if (makebook2025(is, token, engine.get_options()))
+		{
+			Tools::ProgressBar::enable(false);
 			return;
+		}
 
 		// いずれのコマンドも処理しなかったので、使用方法を出力しておく。
 
 		cout << "usage" << endl;
 		cout << "> makebook peta_shock book.db user_book1.db" << endl;
+		Tools::ProgressBar::enable(false);
 	}
 
 } // namespace Book
